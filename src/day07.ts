bf0d3db60962ed1fa9f529e8845e44ecafaaccf6
Norbert Vígh <norbertusvigh@gmail.com>
2025-12-07 10:42:19 +0100

    simplify day 7 implementation

--- src/day07.ts
+++ src/day07.ts
@@ -1,19 +1,19 @@
-import { distinct, sum } from './utils';
+import { sum } from './utils';
 
 const START = 'S';
 const SPLIT = '^';
 
 export const first = (s: string) => {
-	let rays: number[] = [];
+	let rays: Set<number> = new Set();
 	return s
 		.split('\n')
 		.map((line) => {
 			let splits = 0;
 			line.split('').map((c, i) => {
-				if (c === START) rays.push(i);
-				if (c === SPLIT && rays.includes(i)) {
+				if (c === START) rays.add(i);
+				if (c === SPLIT && rays.has(i)) {
 					splits++;
-					rays = [...rays.filter((r) => r !== i), i - 1, i + 1];
+					rays.delete(i) && rays.add(i - 1) && rays.add(i + 1);
 				}
 			});
 			return splits;
@@ -22,20 +22,16 @@ export const first = (s: string) => {
 };
 
 export const second = (s: string) => {
-	return s
-		.split('\n')
-		.reduce(
-			(worlds, line) =>
-				line.split('').reduce((w, c, i) => {
-					if (c === START) w[i] = 1;
-					if (c === SPLIT && w[i] > 0) {
-						w[i - 1] += w[i];
-						w[i + 1] += w[i];
-						w[i] = 0;
-					}
-					return w;
-				}, worlds),
-			[...Array(s.split('\n')[0].length)].map(() => 0)
-		)
-		.reduce(sum);
+	const worlds = [...Array(s.split('\n')[0].length)].map(() => 0);
+	s.split('\n').map((line) =>
+		line.split('').map((c, i) => {
+			if (c === START) worlds[i] = 1;
+			if (c === SPLIT && worlds[i] > 0) {
+				worlds[i - 1] += worlds[i];
+				worlds[i + 1] += worlds[i];
+				worlds[i] = 0;
+			}
+		})
+	);
+	return worlds.reduce(sum);
 };
